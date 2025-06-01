55083ae96c930a736ed1c81486c53b99076574dc
RIYA CHAWLA <riyachawla203@gmail.com>
2025-06-01 19:51:18 -0400

    lab4

--- nextjs-dashboard/nextjs-dashboard/app/api/seed/route.ts
+++ nextjs-dashboard/nextjs-dashboard/app/api/seed/route.ts
@@ -1,28 +1,28 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
-import { invoices, customers, revenue, users } from '../../app/lib/placeholder-data';
+import { invoices, customers, revenue, users } from '@/app/lib/placeholder-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await sql`
-    CREATE TABLE IF NOT EXISTS users (
+  await `sqlCREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
+  await sql
+    `CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL
-    );
-  `;
+    );`
+  ;
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
-      return sql`
-        INSERT INTO users (id, name, email, password)
+      return sql
+       ` INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
+        ON CONFLICT (id) DO NOTHING;`
+      ;
     }),
   );
 
@@ -30,25 +30,25 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
 
-  await sql`
-    CREATE TABLE IF NOT EXISTS invoices (
+  await sql
+   ` CREATE TABLE IF NOT EXISTS invoices (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       customer_id UUID NOT NULL,
       amount INT NOT NULL,
       status VARCHAR(255) NOT NULL,
       date DATE NOT NULL
-    );
-  `;
+    );`
+  ;
 
   const insertedInvoices = await Promise.all(
     invoices.map(
-      (invoice) => sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
+      (invoice) => sql
+       `INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
+        ON CONFLICT (id) DO NOTHING;`
+      ,
     ),
   );
 
@@ -56,24 +56,24 @@ async function seedInvoices() {
 }
 
 async function seedCustomers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
 
-  await sql`
-    CREATE TABLE IF NOT EXISTS customers (
+  await sql
+    `CREATE TABLE IF NOT EXISTS customers (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) NOT NULL,
       image_url VARCHAR(255) NOT NULL
-    );
-  `;
+    );`
+  ;
 
   const insertedCustomers = await Promise.all(
     customers.map(
-      (customer) => sql`
-        INSERT INTO customers (id, name, email, image_url)
+      (customer) => sql
+        `INSERT INTO customers (id, name, email, image_url)
         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
+        ON CONFLICT (id) DO NOTHING;`
+      ,
     ),
   );
 
@@ -81,20 +81,20 @@ async function seedCustomers() {
 }
 
 async function seedRevenue() {
-  await sql`
-    CREATE TABLE IF NOT EXISTS revenue (
+  await sql
+    `CREATE TABLE IF NOT EXISTS revenue (
       month VARCHAR(4) NOT NULL UNIQUE,
       revenue INT NOT NULL
-    );
-  `;
+    );`
+  ;
 
   const insertedRevenue = await Promise.all(
     revenue.map(
-      (rev) => sql`
-        INSERT INTO revenue (month, revenue)
+      (rev) => sql
+        `INSERT INTO revenue (month, revenue)
         VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `,
+        ON CONFLICT (month) DO NOTHING;`
+      ,
     ),
   );
 
@@ -114,4 +114,4 @@ export async function GET() {
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
-}
+} 
